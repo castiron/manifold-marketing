6d97c2e165ec9f87295cd433acedc1042ae41da1
Lucas Thurston <lucas@castironcoding.com>
2017-10-05 11:54:55 -0700

    [B] Add missing config for stg

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :stage, :staging
 set :branch, ENV['REVISION'] || ENV['BRANCH_NAME'] || 'master'
-set :linked_dirs, %w{themes/manifold-marketing/content themes/manifold-marketing/meta storage/framework/sessions storage/logs storage/app/media storage/app/uploads}
+set :linked_dirs, %w{themes/manifold-marketing/content themes/manifold-marketing/meta storage/framework/sessions storage/docs storage/logs storage/app/media storage/app/uploads}
 set :linked_files, %w{.env .warewolf/config www/robots.txt}
 server 'marketing.staging.manifoldapp.org', user: 'manifold_marketing', roles: %w(app db web)
