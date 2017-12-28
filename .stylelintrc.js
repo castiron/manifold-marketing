ce4171db6dd2f2f9ee6080691da59cbaef19ede1
blnkt <blake@castironcoding.com>
2017-12-28 12:02:48 -0800

    [C] Update stylelinter config: ignore plugins dir

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: ["stylelint-scss"],
   extends: "stylelint-config-recess-order",
-  ignoreFiles: [`vendor/**/*`, `/themes/manifold-marketing/assets/stylesheets/lib/*`],
+  ignoreFiles: [`vendor/**/*`, `plugins/**/*`, `themes/manifold-marketing/assets/stylesheets/lib/*`],
   defaultSeverity: "warning",
   rules: {
     "function-calc-no-unspaced-operator": true,
